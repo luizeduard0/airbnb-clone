4257bb15d89c8cd0a8ab6d80fa5a0dfab79757cf
Luiz Eduardo Figueiredo <lerfbr@gmail.com>
2023-10-30 19:55:51 -0300

    Creating Reservations

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -2,14 +2,16 @@ import getCurrentUser from "@/app/actions/getCurrentUser"
 import getListingById from "@/app/actions/getListingById"
 import ClientOnly from "@/app/components/ClientOnly"
 import ListingClient from "./ListingClient"
+import getReservations from "@/app/actions/getReservations"
 
 interface IParams {
   listingId: string
 }
 
 export default async function ListingPage({ params }: { params: IParams }) {
-  const listing = await getListingById(params)
   const currentUser = await getCurrentUser()
+  const listing = await getListingById(params)
+  const reservations = await getReservations(params)
 
   if(!listing) {
     return (
@@ -24,6 +26,7 @@ export default async function ListingPage({ params }: { params: IParams }) {
       <ListingClient
         listing={listing}
         currentUser={currentUser!}
+        reservations={reservations}
       />
     </ClientOnly>
   )
