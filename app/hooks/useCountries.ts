b7d4e386d779bbd63c59b5435302c444c796309f
Luiz Eduardo Figueiredo <lerfbr@gmail.com>
2023-10-30 09:24:06 -0300

    Location Step with Country Map

---
+++ app/hooks/useCountries.ts
@@ -0,0 +1,24 @@
+import countries from "world-countries";
+
+const formattedCountries = countries.map((country) => ({
+  label: country.name.common,
+  value: country.cca2,
+  flag: country.flag,
+  latlng: country.latlng,
+  region: country.region,
+}));
+
+const useContries = () => {
+  const getAll = () => formattedCountries;
+
+  const getByValue = (value: string) => {
+    return formattedCountries.find((country) => country.value === value);
+  };
+
+  return {
+    getAll,
+    getByValue,
+  };
+};
+
+export default useContries;
