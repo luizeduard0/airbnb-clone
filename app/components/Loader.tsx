9e6afe3a060902abdf6b70c4e545f9c9f8e8a1a4
Luiz Eduardo Figueiredo <lerfbr@gmail.com>
2023-10-31 16:12:48 -0300

    Loader

---
+++ app/components/Loader.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { PuffLoader } from "react-spinners"
+
+export default function Loader() {
+  return (
+    <div className="
+      h-[70vh]
+      flex flex-col
+      justify-center items-center
+    ">
+      <PuffLoader color="red" size={100} />
+    </div>
+  )
+}
